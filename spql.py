6912afbd9a4acde37c0a7a85adf8ddeda26dca06
horia <horia_zegheru@yahoo.com>
2019-01-12 18:25:55 +0200

    aws added query in eng + symptoms

--- spql.py
+++ spql.py
@@ -56,17 +56,38 @@ def query_by_contains_name(disease_name_ro):
 	PREFIX owl:  <http://www.w3.org/2002/07/owl#>
 	PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 	PREFIX purl: <http://www.purl.org/>
+	PREFIX obo: <http://purl.obolibrary.org/obo/>
 	prefix oboInOwl: <http://www.geneontology.org/formats/oboInOwl#>
 	prefix purlObo: <http://purl.obolibrary.org/obo/doid.owl#>
 
-	SELECT DISTINCT ?s ?label ?trans
+	SELECT DISTINCT ?s ?label ?trans ?symptom
 	WHERE {
 		   ?s a owl:Class .
 		   ?s rdfs:label ?label .
   		   ?s purlObo:ro_translation ?trans .
+		   ?s obo:IAO_0000115 ?symptom .
   			FILTER regex(?trans, "%s", "i" ) 
 	}
 	""" % disease_name_ro
+
+def query_by_contains_name_en(disease_name_en):
+	return """ 
+		PREFIX owl:  <http://www.w3.org/2002/07/owl#>
+		PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+		PREFIX purl: <http://www.purl.org/>
+		PREFIX obo: <http://purl.obolibrary.org/obo/>
+		prefix oboInOwl: <http://www.geneontology.org/formats/oboInOwl#>
+		prefix purlObo: <http://purl.obolibrary.org/obo/doid.owl#>
+
+		SELECT DISTINCT ?s ?label ?trans ?symptom
+		WHERE {
+			   ?s a owl:Class .
+			   ?s rdfs:label ?label .
+	  		   ?s purlObo:ro_translation ?trans .
+			   ?s obo:IAO_0000115 ?symptom .
+	  			FILTER regex(?label, "%s", "i" ) 
+		}
+		""" % disease_name_en
 
 @app.route('/')
 def index():
@@ -76,6 +97,10 @@ def index():
 @app.route("/select" , methods=['GET', 'POST'])
 def select():
 	disease_name = request.args.get("search_box")
+
+	if disease_name == '':
+		return render_template('error.html')
+
 	query_string = query_by_contains_name(disease_name)
 	json_string = query(query_string, "", sparql_service)
 	resultset = json.loads(json_string)
@@ -84,12 +109,41 @@ def select():
 	for result in resultset["results"]["bindings"]:
 		label = result["label"]["value"]
 		syn = result["trans"]["value"]
+		symptoms = result["symptom"]["value"]
 
-		label_trans.append((label, syn))
+		label_trans.append((label, syn, symptoms))
 
 	html_page = render_template("diseases_by_name.html", **{
 			'query': disease_name,
+			'language': 'Search was made in romanian',
 			'diseases': label_trans,
+	})
+
+	return html_page
+
+@app.route("/select_en" , methods=['GET', 'POST'])
+def select_en():
+	disease_name = request.args.get("search_box")
+
+	if disease_name == '':
+		return render_template('error.html')
+
+	query_string = query_by_contains_name_en(disease_name)
+	json_string = query(query_string, "", sparql_service)
+	resultset = json.loads(json_string)
+
+	label_trans = []
+	for result in resultset["results"]["bindings"]:
+		label = result["label"]["value"]
+		syn = result["trans"]["value"]
+		symptoms = result["symptom"]["value"]
+
+		label_trans.append((label, syn, symptoms))
+
+	html_page = render_template("diseases_by_name.html", **{
+		'query': disease_name,
+		'language': 'Search was made in english',
+		'diseases': label_trans,
 	})
 
 	return html_page
@@ -136,7 +190,7 @@ if __name__ == "__main__":
 	# trebuie facut query-ul care scoate toate sinonimele si le
 	# imperecheaza cu ce e deja tradus)
 
-	init()
+	# init()
 
 	app.secret_key = os.urandom(12)
 	app.run(debug=True)
